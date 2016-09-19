c81abacd9e2a6f9bab006b85365242516b3a5a0a
Leon Duan <leonduantian@hotmail.com>
2016-09-19 09:18:06 -0500

    email cron job working

--- src/edu/utexas/ece/metablog/CronServlet.java
+++ src/edu/utexas/ece/metablog/CronServlet.java
@@ -2,35 +2,58 @@ package edu.utexas.ece.metablog;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Logger;
-import javax.mail.*;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
+import javax.mail.Transport;
 
 @SuppressWarnings("serial")
-public class CronServlet extends HttpServlet{
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		try
-		{
-			Properties props = new Properties();
-			Session session = Session.getDefaultInstance(props, null);
-			Message msg = new MimeMessage(session);
-			msg.setFrom(new InternetAddress("leonduantian@hotmail.com", "Leon Duan"));
-			msg.addRecipient(Message.RecipientType.TO,
-	                   new InternetAddress("leonduantian@hotmail.com", "Mr. User"));
-			msg.setSubject("Daily Digest");
-			msg.setText("Test");
-			Transport.send(msg);
-	    } catch (AddressException e) {
-	    	  // ...
-	    } catch (MessagingException e) {
-	      // ...
-	    } catch (UnsupportedEncodingException e) {
-	      // ...
-	    }
-	}
+public class CronServlet extends HttpServlet {
+	private static Map<String, String> customer_List = new HashMap();
 	
+	
+	private static final Logger _logger = Logger.getLogger(CronServlet.class.getName());
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+//		try {
+////				_logger.info("Cron Job has been executed");
+//				Properties props = new Properties();
+//				Session session = Session.getDefaultInstance(props, null);
+//				Message msg = new MimeMessage(session);
+//				msg.setFrom(new InternetAddress("jokerjoker216@gmail.com"));
+//				msg.addRecipient(Message.RecipientType.TO, new InternetAddress("leonduantian@hotmail.com"));
+//				msg.setSubject("Test");
+//				msg.setText("Test");
+//				Transport.send(msg);
+//			}
+//		catch (AddressException e) 
+//		{
+//			e.printStackTrace();
+//			_logger.warning("Email failed");
+//		} 
+//		catch (MessagingException e) 
+//		{
+//			e.printStackTrace();
+//			_logger.warning("Email failed");
+//		} 
+//		catch (Exception e)
+//		{
+//			e.printStackTrace();
+//			_logger.warning("Email failed");
+//		}
+	}
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		doGet(req, resp);
+	}
 }
